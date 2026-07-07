05e44a21a2fe39b0deac4ca2e1caef80e18fc17c
Pragyansh Vardhan <pragyansh.uxai@gmail.com>
2026-07-07 21:31:33 +0100

    results: Run 5 (8B, T0.0) - Sharpe 2.485, first correct BA.L short

--- run_experiment.py
+++ run_experiment.py
@@ -61,13 +61,13 @@ def execute_experiment(model_name, temperature, ticker_count, run_name):
 
 if __name__ == "__main__":
     MODEL = "llama-3.1-8b-instant"
-    TEMP = 0.2
+    TEMP = 0.0
     TICKERS = 5
 
     os.environ["EXPERIMENT_MODEL"] = MODEL
     os.environ["EXPERIMENT_TEMP"] = str(TEMP)
 
 
-    RUN_NAME = f"Run4_{MODEL}_T{TEMP}_{TICKERS}Assets"
+    RUN_NAME = f"Run5_{MODEL}_T{TEMP}_{TICKERS}Assets"
     os.environ["RUN_NAME"] = RUN_NAME
-    execute_experiment(MODEL, TEMP, TICKERS, RUN_NAME)
+    execute_experiment(MODEL, TEMP, TICKERS, RUN_NAME) 
